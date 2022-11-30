4ea64f9079a739d3dfcb0877493479b013acc1e5
Joel Perez <joel_perez65@yahoo.com>
2022-11-29 18:24:42 -0600

    fixed config file

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,8 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
 })
-    .then(() => console.log("Connection to the database is good to go!"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err))
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
+
